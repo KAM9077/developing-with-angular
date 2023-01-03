d6fabccd48bb5676951e3a3beb9f245a72d3c119
KAM9077 <kamalhajjar907@gmail.com>
2023-01-03 21:01:08 +0300

    update

--- angular/components/dynamic-content/src/app/app.component.ts
+++ angular/components/dynamic-content/src/app/app.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
 
-  listItems: Array<{title: string}> = [
+
+  listItems: Array<{ title: string }> = [
     { title: 'item 1' },
     { title: 'item 2' },
     { title: 'item 3' }
   ]
 
   context: { text: string } = {
-      text: 'test'
+    text: 'test'
   }
+
+
 }
--- angular/components/dynamic-content/src/app/templated-list/templated-list.component.ts
+++ angular/components/dynamic-content/src/app/templated-list/templated-list.component.ts
@@ -9,6 +9,13 @@ export class TemplatedListComponent {
   @ContentChild(TemplateRef)
   template: TemplateRef<any>;
 
-  @Input()
-  items: Array<{ title: string }> = [];
+  // @Input() items: Array<{ title: string }> = [];
+
+  items: Array<{ title: string }> = [
+    { title: 'item 1' },
+    { title: 'item 2' },
+    { title: 'item 3' }
+  ]
+
+  @Input('selector') aaa: TemplateRef<any> | undefined
 }
